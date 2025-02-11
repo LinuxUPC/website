f0a43a53ea18f1e28737bb6446948ee6a0470366
Pandax40 <pandaxsantin@gmail.com>
2025-02-11 23:12:47 +0100

    component loading code optimized and refactored

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,18 +1,18 @@
 // Load a component into a selector
-function loadComponent(selector, file, callback) {
+function loadComponent(selector, file) {
     fetch(file)
         .then(response => response.text())
         .then(data => {
-            document.querySelector(selector).innerHTML = data;
-
-            if (callback) { callback(); } // Recursively load more components
+            document.querySelectorAll(selector).forEach(element => {
+                element.innerHTML = data;
+            });
         })
         .catch(error => console.error(`Error loading component in ${file}:`, error));
 }
 
 // Load components on page load event
 document.addEventListener("DOMContentLoaded", () => {
-    loadComponent(".title-component", "./components/title.html", () => {
-        loadComponent(".window-component", "./components/window.html");
-    });
+    loadComponent(".title-component", "./components/title.html");
+    loadComponent(".about-component", "./components/about.html");
+    loadComponent(".window-component", "./components/window.html");
 });
