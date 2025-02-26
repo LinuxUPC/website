c1cbb2d10face8fa570b1d75e64423a97a564f1d
panda <pandaxsantin@gmail.com>
2025-02-26 09:32:24 +0100

    fix: update the color of the scroll bar depending on the color mode

--- assets/js/colorMode.js
+++ assets/js/colorMode.js
@@ -1,16 +1,23 @@
 let lightMode = localStorage.getItem('lightMode');
 const toggleModeButton = document.querySelector('#colorModeButton');
 
-if(lightMode === 'enabled') 
+if(lightMode === 'enabled'){
     document.body.classList.remove('dark');
+    document.documentElement.style.setProperty('color-scheme', 'light');
+}
+else 
+    document.documentElement.style.setProperty('color-scheme', 'dark');
+    
 
 
 function toggleColorMode() {
     if(lightMode === 'enabled') {
         document.body.classList.add('dark');
+        document.documentElement.style.setProperty('color-scheme', 'dark');
         lightMode = null;
     } else {
         document.body.classList.remove('dark');
+        document.documentElement.style.setProperty('color-scheme', 'light');
         lightMode = 'enabled';
     }
     localStorage.setItem('lightMode', lightMode);
