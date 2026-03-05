446a1900c1eedf42d67fac7e1245abde4b8968e6
marc-marcos <marc-marcos@users.noreply.github.com>
2026-03-05 07:54:09 +0000

    Deploy preview for PR 18 🛫

---
+++ pr-preview/pr-18/assets/js/sidebar.js
@@ -0,0 +1,26 @@
+const sidebar = document.getElementById("sidebar");
+const sidebarButton = document.getElementById("sidebarCollapse");
+sidebarButton.addEventListener("click", () => {
+  sidebar.classList.toggle("show");
+  adjustSidebarTop();
+  sidebarButton.blur();
+});
+
+function adjustSidebarTop() {
+  const topBar = document.getElementById("topbar");
+  let topBarHeight = topBar.clientHeight;
+
+  if (getComputedStyle(sidebarButton).display != "none" && !sidebar.classList.contains("show")) {
+    const remInPixels = parseFloat(
+      getComputedStyle(document.documentElement).fontSize
+    );
+    topBarHeight += 0.5 * remInPixels;
+  }
+
+  sidebar.style.top = `${topBarHeight}px`;
+  sidebar.style.maxHeight = `calc(100vh - ${topBarHeight}px)`;
+}
+
+adjustSidebarTop();
+window.addEventListener("resize", adjustSidebarTop);
+window.addEventListener("scroll", adjustSidebarTop);
